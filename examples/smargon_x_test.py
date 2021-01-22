f325e7c2f1c69650a68b2ddbd2facb8e067e85b9
Nader Afshar <nadera@ansto.gov.au>
2021-01-22 16:37:48 +1100

    chnages after branching mainly to be ignored

--- examples/smargon_x_test.py
+++ examples/smargon_x_test.py
@@ -1,19 +1,31 @@
-
+from examples import smargon_code_templates
 import os
 from time import sleep
 import yaml as ym
 import regex as re
 
 """ example context managed powerpmac read"""
-from ppmac import GpasciiClient
+from wrasc import ppmac_ra as ppra
 
-ppmac_test_IP = os.environ["PPMAC_TEST_IP"]
-p4_root = "/beamline/perforce"
-source_file_path = os.path.normpath(r"opa/mx1/ctrls/SR03BM01MCS22/Project/PMAC Script Language/PLC Programs")
+ppmac_test_IP = "10.109.25.22"
+p4_root = os.path.normpath(r"C:\Users\ics\p4c\opa\mx1\ctrls\SR03BM01MCS22")
+source_file_path = os.path.normpath(r"Project/PMAC Script Language/PLC Programs")
 
-def stage_and_verify(ppmac=None, cmds=None, manual_learning=False, verify_conds=None, base_dir="", stage_name=None, base_ext=".ppmc"):
-   
+
+def stage_and_verify(
+    ppmac=None,
+    cmds=None,
+    manual_learning=False,
+    verify_conds=None,
+    base_dir="",
+    stage_name=None,
+    base_ext=".ppmc",
+):
+
     stage_counter = 0
+
+    if isinstance(cmds, str):
+        cmds = cmds.splitlines()
 
     assert isinstance(cmds, list)
     if stage_name:
@@ -25,8 +37,8 @@ def stage_and_verify(ppmac=None, cmds=None, manual_learning=False, verify_conds=
     stage_counter += 1
 
     _filename = _filename.replace(" ", "_").replace(",", "-").replace(".", "-")
-    _filename = re.sub('[^\w\s-]', '', _filename).strip()
-    legit_filename = re.sub('[-\s]+', '-', _filename)
+    _filename = re.sub("[^\w\s-]", "", _filename).strip()
+    legit_filename = re.sub("[-\s]+", "-", _filename)
     base_filename = os.path.join(base_dir, legit_filename)
     ref_filename = base_filename + base_ext + ".ref"
     dump_filename = base_filename + base_ext + ".loaded"
@@ -38,27 +50,38 @@ def stage_and_verify(ppmac=None, cmds=None, manual_learning=False, verify_conds=
     if manual_learning:
 
         # overwrite the ref file
-        with open(ref_filename, 'w+') as reffile:
+        with open(ref_filename, "w+") as reffile:
             ym.dump(tpl, reffile, default_flow_style=False)
-            reffile.close()        
+            reffile.close()
 
     elif os.path.exists(ref_filename):
 
-        with open(ref_filename, 'r') as reffile:
+        with open(ref_filename, "r") as reffile:
             loaded_ref = ym.load(reffile, Loader=ym.FullLoader)
             reffile.close()
-        
+
         if tpl != loaded_ref:
-            with open(dump_filename, 'w+') as reffile:
+            with open(dump_filename, "w+") as reffile:
                 ym.dump(tpl, reffile, default_flow_style=False)
-                reffile.close()            
-            raise RuntimeError("ERROR encountered staging " + stage_name + " stage. Compare " + dump_filename + " vs " + ref_filename)
+                reffile.close()
+            raise RuntimeError(
+                "ERROR encountered staging "
+                + stage_name
+                + " stage. Compare "
+                + dump_filename
+                + " vs "
+                + ref_filename
+            )
     else:
         raise RuntimeError("No reference file for stage" + stage_name)
 
+    action_msg = (
+        "staged and confirmed" if not manual_learning else "staged and snapshot"
+    )
+
     if verify_conds is None:
-        return stage_name + " -- staged and confirmed."
-    
+        return stage_name + f" -- {action_msg}."
+
     # the stage commands respons is matching, now verify the conditions
 
     assert isinstance(verify_conds, list)
@@ -80,56 +103,105 @@ def stage_and_verify(ppmac=None, cmds=None, manual_learning=False, verify_conds=
 
         assert eval(verify_text) is True
 
-    return f"{stage_name} -- staged, confirmed and {len(verify_conds)} conditions verified."
-
-#  export PPMAC_TEST_IP="10.23.92.220"
-
-with GpasciiClient(ppmac_test_IP) as smargon:
-    smargon.connect()
-    # while True:
-
-    # setp 1 : preparation of the test
+    return f"{stage_name} -- {action_msg} and {len(verify_conds)} conditions verified."
 
 
-    def load_from_file(plc_file_name = "") :
+smargon_ppmac = ppra.PPMAC(host=ppmac_test_IP, backward=True)
+smargon_ppmac.connect()
+assert smargon_ppmac.connected
 
-        manual_learning = True
-        
-        plc_file = os.path.join(p4_root, source_file_path, plc_file_name)
+# setp 1 : preparation of the test
 
-        if not os.path.exists(plc_file):
-            raise RuntimeError(f"loading file not found: {plc_file}")
 
-        with open(plc_file, 'r') as plcfile:
-            plc_code = plcfile.read()
-            cmds = [plc_code]
-            plcfile.close
+def load_from_file(plc_file_name="", manual_learning=False):
 
-        print(stage_and_verify(smargon, stage_name=f"loading {plc_file_name}", cmds=cmds, manual_learning=manual_learning))
-    
-    load_from_file("30_test_set.plc")
-    load_from_file("31_test_unset.plc")
+    plc_file = os.path.join(p4_root, source_file_path, plc_file_name)
 
-    manual_learning = True
+    if not os.path.exists(plc_file):
+        raise RuntimeError(f"loading file not found: {plc_file}")
 
-    cmds = ["LIST PLC 30,0,100", "LIST PLC 31,0,100", "Motor[3].AuxFault", "Motor[3].JogSpeed", "Motor[3].DesVelZero"]
-    verifies = [["#3p", ">", 980]]
-    print(stage_and_verify(smargon, stage_name="initial", cmds=cmds, manual_learning=manual_learning, verify_conds=verifies))
+    with open(plc_file, "r") as plcfile:
+        plc_code = plcfile.read()
+        cmds = [plc_code]
+        plcfile.close
 
-    # setp 2 : loop
-    
-    manual_learning = True
+    print(
+        stage_and_verify(
+            smargon_ppmac,
+            stage_name=f"loading {plc_file_name}",
+            cmds=cmds,
+            manual_learning=manual_learning,
+        )
+    )
 
-    cmds = ["enable PLC 30"]
-    verifies =[["Motor[3].pAuxFault", "==", "Acc65E[0].DataReg[0].a"]]
-    print(stage_and_verify(smargon, stage_name="set_protection", cmds=cmds, manual_learning=manual_learning, verify_conds=verifies))
 
-    manual_learning = False
+print(
+    stage_and_verify(
+        smargon_ppmac,
+        stage_name=f"loading plc10",
+        cmds=smargon_code_templates.plc10_code,
+        manual_learning=False,
+    )
+)
 
-    exit(0)
-    cmds = ["#3 j: -1100", "Motor[3].pAuxFault"]
-    print(stage_and_verify(smargon, stage_name="set_move", cmds=cmds, manual_learning=manual_learning))
 
+print(
+    stage_and_verify(
+        smargon_ppmac,
+        stage_name=f"loading prg10",
+        cmds=smargon_code_templates.prog10_code,
+        manual_learning=False,
+    )
+)
+
+
+exit(0)
+
+manual_learning = True
+
+cmds = [
+    "LIST PLC 30,0,100",
+    "LIST PLC 31,0,100",
+    "Motor[3].AuxFault",
+    "Motor[3].JogSpeed",
+    "Motor[3].DesVelZero",
+]
+verifies = [["#3p", ">", 980]]
+print(
+    stage_and_verify(
+        smargon_ppmac,
+        stage_name="initial",
+        cmds=cmds,
+        manual_learning=manual_learning,
+        verify_conds=verifies,
+    )
+)
+
+# setp 2 : loop
+
+manual_learning = True
+
+cmds = ["enable PLC 30"]
+verifies = [["Motor[3].pAuxFault", "==", "Acc65E[0].DataReg[0].a"]]
+print(
+    stage_and_verify(
+        smargon_ppmac,
+        stage_name="set_protection",
+        cmds=cmds,
+        manual_learning=manual_learning,
+        verify_conds=verifies,
+    )
+)
+
+manual_learning = False
+
+exit(0)
+cmds = ["#3 j: -1100", "Motor[3].pAuxFault"]
+print(
+    stage_and_verify(
+        smargon_ppmac, stage_name="set_move", cmds=cmds, manual_learning=manual_learning
+    )
+)
 
 
 print("complete.")
