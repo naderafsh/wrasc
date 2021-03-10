8f9221a5ce3015ac07867200d860f461ccefdb21
Sandeep Nuthalapati <nuthalas@ansto.gov.au>
2021-03-10 17:50:07 +1100

    Updated command line code to use typer.

---
+++ commands/sg_m5_repeat_command_typer.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+import typer
+from datetime import datetime, timedelta
+from time import time, sleep, ctime
+import logging
+from pathlib import Path
+import subprocess
+import json
+import re
+
+
+def run(total_time: int, sleep_time: int):
+    current_datetime = datetime.now()
+    total_run_time = current_datetime + timedelta(minutes=total_time)
+    sg_m5_repeat_path = Path.cwd().joinpath('examples', 'sg_m5_repeat.py')
+
+    # Prepare filepath to store logs
+    logs_file_path = Path.cwd().joinpath(
+        'logs',
+        'm5-sequence-run'
+    )
+    # Create the folder to store logs
+    logs_file_path.mkdir(parents=True, exist_ok=True)
+
+    # Set the basic config for logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        filename=f'{logs_file_path.as_posix()}/{current_datetime.strftime("%Y%m%d-%H%M%S")}.log'
+    )
+
+    print(
+        f'This process is initiated at {ctime(time())}\n'
+    )
+
+    logging.info(
+        f'This process is initiated at {ctime(time())}'
+    )
+
+    # Count to log how many times a full run executed.
+    i = 1
+    while current_datetime <= total_run_time:
+        '''
+        Subprocess run will return a returncode.
+        0 - Successfully ran the subprocess
+        1 - Failed subprocess
+        '''
+        start_run = ctime(time())
+
+        # Logging start of subprocess.
+        print(
+            '======================================\n'
+            f'Run id: {i}\n'
+            f'Subprocess started at {start_run}\n'
+        )
+
+        logging.info(json.dumps({
+            'run_id': i,
+            'message': f'Subprocess started at {start_run}'
+        }))
+
+        process = subprocess.run(
+            ['python', sg_m5_repeat_path.as_posix()],
+            capture_output=True,
+            shell=True
+        )
+
+        # Check whether the subprocess status
+        if process.returncode:
+            process_error = process.stderr.decode('utf-8')
+            print(process_error)
+            logging.error(process_error)
+            typer.Abort()
+
+        # Logging end of subprocess.
+        end_run = ctime(time())
+        print(
+            f'Run id: {i}\n'
+            f'Subprocess ended at {end_run}\n'
+            '===================================='
+        )
+
+        logging.info(json.dumps({
+            'run_id': i,
+            'message': f'Subprocess ended at {end_run}'
+        }))
+
+        # Sleep for next iteration. Convert hours to seconds.
+        sleep_time_secs = sleep_time * 60
+        with typer.progressbar(
+            range(10),
+            length=sleep_time_secs,
+            label='In Sleep'
+        ) as progress:
+            for i in progress:
+                sleep(sleep_time_secs)
+
+        # Increment the count
+        i += 1
+
+
+def main(
+    time: int = typer.Option(720, help='Total time(in minutes) of the run'),
+    sleep: int = typer.Option(60, help='Sleep time(in minutes) of the run')
+):
+    """Run icing motors tests
+    """
+    # Run the process
+    run(time, sleep)
+
+
+if __name__ == '__main__':
+    typer.run(main)
